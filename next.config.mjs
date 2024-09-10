f9391005ab94f8af537c2184234d98fc8874214a
Edson Yamble <edsonyamble2@gmail.com>
2024-09-10 13:41:47 +0700

    next confug js settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //work env file
+	env: {
+		APP_ENV: process.env.APP_ENV,
+		SERVER_URL: process.env.SERVER_URL,
+		APP_URL: process.env.APP_URL,
+		APP_DOMAIN: process.env.APP_DOMAIN
+	},
+  //image  с сторону сервиси 
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'avatars.yandex.net'
+			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com'
+			}
+		]
+	},
+  //функция для загружение картинке из сервере
+  async rewrites() {
+    return [
+        {
+            source: '/uploads/:path*',
+            destination: `${process.env.SERVER_URL}/uploads/:path*`,
+        },
+    ]
+}
+}
 
-export default nextConfig;
+export default nextConfig
